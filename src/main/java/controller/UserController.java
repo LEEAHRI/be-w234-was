9ef45449469871e67038eb50560e41fa9b8857b3
이아리 <kakao@egg-boiled.local>
2022-09-19 01:20:35 +0900

    UserController 생성

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import factory.ResponseFactory;
+import model.Request;
+import model.Response;
+import service.UserService;
+
+public class UserController {
+    private UserService userService;
+
+    private Response createUser(Request request) {
+        userService.create(request);
+        return ResponseFactory.create200OkResponse();
+    }
+
+    private Response updateUser(Request request) {
+        userService.update(request);
+        return ResponseFactory.create200OkResponse();
+    }
+
+    public Response routeUserRequest(Request request) {
+        if (request.getUrl().equals("/user/create")) {
+            return createUser(request);
+        }
+        if (request.getUrl().equals("/user/update")) {
+            return updateUser(request);
+        }
+        return null;
+    }
+}
