4b7bd73506c3aa39fa18db6ac3d4b5617846e6a4
이아리 <kakao@egg-boiled.local>
2022-09-19 01:24:20 +0900

    model.request 생성

---
+++ src/main/java/model/Request.java
@@ -0,0 +1,21 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class Request {
+    private String method;
+    private String url;
+    private String queryString;
+    private String protocol;
+    private String host;
+    private String connection;
+    private String allowOrigins;
+
+    public Request(String method, String url, String queryString, String protocol) {
+        this.method = method;
+        this.url = url;
+        this.queryString = queryString;
+        this.protocol = protocol;
+    }
+}
