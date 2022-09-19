9f59a7129ea99f430b2c55d10694956d1638089d
LEEAHRI <dkfl4059@gmail.com>
2022-09-19 23:40:22 +0900

    UserController 수정 | POST 기능 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,33 +1,56 @@
 package controller;
 
-import factory.RequestFactory;
 import factory.ResponseFactory;
+import factory.ResponseFactory;
+import factory.UserFactory;
 import model.Request;
 import model.Response;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import service.UserService;
+import util.ResourceUtils;
 
-import java.util.Map;
+import java.util.List;
 
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private UserService userService;
 
+    public UserController() {
+        userService = new UserService();
+    }
+
     private Response createUser(Request request) {
-        userService.create(request);
+        User user = UserFactory.createUserByBodyParam(request);
+        userService.create(user);
         return ResponseFactory.create200OkResponse();
     }
 
-//    private Response updateUser(Request request) {
-//        userService.update(request);
-//        return ResponseFactory.create200OkResponse();
-//    }
+    private Response getUser(Request request) {
+        User user = UserFactory.createUserByQueryString(request);
+        userService.create(user);
+        return ResponseFactory.create200OkResponse();
+    }
+
+    private Response serveResources(Request request) {
+        Response response = new Response();
+        byte[] body = ResourceUtils.readFile(request.getUrl());
+        response.setContentType("html");
+        response.setBody(body);
+        return response;
+    }
 
     public Response routeUserRequest(Request request) {
-        if (request.getUrl().equals("/user/create")) {
+        if (request.getUrl().equals("/user/create") && request.getMethod().equals("POST")) {
             return createUser(request);
         }
-//        if (request.getUrl().equals("/user/update")) {
-//            return updateUser(request);
-//        }
-        return null;
+
+        if (request.getUrl().equals("/user/create") && request.getMethod().equals("GET")) {
+            return getUser(request);
+        }
+
+        return serveResources(request);
     }
 }
