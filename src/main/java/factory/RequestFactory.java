0302f5bdd7153556539eac8627ae37082bf9dac8
LEEAHRI <dkfl4059@gmail.com>
2022-09-19 23:37:13 +0900

    HTTPRequest 파싱하는 RequestFactory 수정

--- src/main/java/factory/RequestFactory.java
+++ src/main/java/factory/RequestFactory.java
@@ -4,13 +4,16 @@ import model.Request;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Map;
 
 public class RequestFactory {
 
     private static final Logger logger = LoggerFactory.getLogger(RequestFactory.class);
+    private static final String BLANK = " ";
 
     public static Request createRequest(Socket connection) {
 
@@ -18,15 +21,25 @@ public class RequestFactory {
             InputStream in = connection.getInputStream();
             // 요구사항 step1-1 : Requsest Header 출력
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String methodAndUrl = br.readLine();
-            String method = methodAndUrl.split(" ")[0];
-            String url = methodAndUrl.split(" ")[1];
-            String queryString = br.readLine();
-            String protocol = br.readLine();
+            String firstLine = br.readLine();
+            String method = firstLine.split(BLANK)[0];
+            String requestTarget = firstLine.split(BLANK)[1];
+            String url = requestTarget.split("\\?")[0];
+            Map<String, String> queryString = null;
+            if (requestTarget.contains("?")) {
+                queryString = HttpRequestUtils.parseQueryString(requestTarget.split("\\?")[1]);
+            }
+            String protocol = firstLine.split(BLANK)[2];
             while (true) {
                 String line = br.readLine();
                 if (StringUtils.isEmpty(line)) break;
                 logger.debug("Line: {}", line);
+            }
+
+            if (method.equals("POST")) {
+                String line = br.readLine();
+                Map<String, String> bodyParam = HttpRequestUtils.parseQueryString(line);
+                return new Request(method, url, queryString, protocol, bodyParam);
             }
             return new Request(method, url, queryString, protocol);
         } catch (IOException e) {
