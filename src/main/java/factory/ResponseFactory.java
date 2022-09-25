7e1805d2fd0125681ea98c46be6f7891288a9b2b
LEEAHRI <dkfl4059@gmail.com>
2022-09-25 15:23:26 +0900

    create302FoundResponse 생성

--- src/main/java/factory/ResponseFactory.java
+++ src/main/java/factory/ResponseFactory.java
@@ -1,7 +1,6 @@
 package factory;
 
 import model.Response;
-
 public class ResponseFactory {
 
     public static Response create200OkResponse() {
@@ -9,10 +8,14 @@ public class ResponseFactory {
         response.setStatus("200");
         return response;
     }
-
     public static Response create500ErrorResponse() {
         Response response = new Response();
         response.setStatus("500");
+        return response;
+    }
+    public static Response create302FoundResponse() {
+        Response response = new Response();
+        response.setStatus("302");
         return response;
     }
 }
